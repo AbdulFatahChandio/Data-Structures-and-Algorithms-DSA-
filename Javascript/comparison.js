6eef01ab03495a4511f89dbdf9b4e6f47338f89b
AbdulFatahChandio <abdulfatahchandio61@gmail.com>
2026-01-28 15:58:38 +0500

    Comparison in JS

---
+++ Javascript/comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// // console.log(2 =! 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null >= 0); // null convert into 0 and 0 >0 ==0
+// console.log(null == 0);
+// console.log(null != 0);
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0); 
+// console.log(undefined == 0);
+// console.log(undefined != 0);
+
+
+
+// === strict check data type and value
+
+// console.log("2"==2);
+// console.log("2"===2);
