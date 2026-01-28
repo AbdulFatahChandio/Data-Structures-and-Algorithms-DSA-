1ee58466d3a30f52825a62fab0af562a6a7e5b41
AbdulFatahChandio <abdulfatahchandio61@gmail.com>
2026-01-28 15:13:36 +0500

    Data Types Learning

---
+++ Javascript/dataTpyes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+// console.log(3 + 3 ); console.log("hitesh"); // bad code readability
+
+
+let name = "abdul fatah" // string
+let age = 18 // number
+let isLoggedIn = true //Boolean
+
+
+// number => 2 to power 53
+// bigInt
+// string => " "
+// boolean => true / false 
+// null => standalone value
+// undefined => not defined
+// symbol  => used for uniqueness
+
+//  Object
+
+console.log(typeof "ali ")
+console.log(typeof (name))
+console.log(typeof  (age))
+console.log(typeof  (isLoggedIn))
+console.log(typeof  (null))
+console.log(typeof  (undefined))
