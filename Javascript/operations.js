8b597b61363d77e5e6ef5d55cc9b2f71e0082cbf
AbdulFatahChandio <abdulfatahchandio61@gmail.com>
2026-01-28 15:42:16 +0500

    Basic operations in Js

---
+++ Javascript/operations.js
@@ -0,0 +1,44 @@
+
+//******************************************** Operations ******************************************/
+
+let value = 3
+let negValue = -value
+
+// console.log(value)
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+// let str1 = "abdul"
+// let str2 = " fatah"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log("1"+(2+2))
+// console.log(1+2+2)
+// console.log(1+2+"2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(-true)
+// console.log(!true)
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// console.log(gameCounter)
+// gameCounter++; //prefix 
+// console.log(gameCounter)
+// ++gameCounter; // postfix
+// console.log(gameCounter)
